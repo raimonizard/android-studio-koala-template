c27fe8c04e214c56d364c33928c76bafbf925be8
raimonizard <raimon.izard@gmail.com>
2024-11-08 10:44:24 +0100

    Initial commit

---
+++ app/src/main/java/com/example/androidstudio_koala_template/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.androidstudio_koala_template
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.androidstudio_koala_template.ui.theme.AndroidStudioKoalaTemplateTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AndroidStudioKoalaTemplateTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true
+    , showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    AndroidStudioKoalaTemplateTheme {
+        Greeting("Android")
+    }
+}
